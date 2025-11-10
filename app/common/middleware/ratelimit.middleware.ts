a0cad63b1691fb1d80df89e43793138bc59f2a0e
Dikshit Rawal <dikshitrawal@Dikshits-MacBook-Air.local>
2025-11-10 18:52:19 +0530

    added limiter on the login route

--- app/common/middleware/ratelimit.middleware.ts
+++ app/common/middleware/ratelimit.middleware.ts
@@ -9,3 +9,12 @@ export const limiter = rateLimit({
   legacyHeaders: false,
   keyGenerator: (req) => req.ip!, // Use the IP as the unique identifier
 });
+
+export const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,        // 1 minute window
+  max: 3,                         // only 5 login attempts per IP per minute
+  message: "Too many login attempts from this IP, please try again later.",
+  standardHeaders: true,          // Return rate limit info in headers
+  legacyHeaders: false,
+  keyGenerator: (req) => req.ip!,  // Use the IP as the unique identifier
+});
--- app/modules/user/user.route.ts
+++ app/modules/user/user.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import * as userController  from "./user.controller"
 import * as userValidation from "./user.validation"
 import { catchError } from "../../common/middleware/catch-error.middleware";
+import { loginLimiter } from "../../common/middleware/ratelimit.middleware";
 const router = Router();
 
 
 router.post("/" , userValidation.signup , catchError  ,userController.createUser);
-router.post("/login" , userValidation.login , catchError , userController.loginUser);
+router.post("/login" , loginLimiter , userValidation.login , catchError , userController.loginUser);
 
 
 export default router ;
